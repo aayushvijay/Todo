cd305b033b4663e7aa1924353051232036450e04
Aayush <aayushvj8699@gmail.com>
2020-09-01 18:16:32 +0530

    Updates

---
+++ precache-manifest.23565b2099e31f32af28232bb854a6af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "019571cae7b1afec52403fa306028907",
+    "url": "/Todo/index.html"
+  },
+  {
+    "revision": "eb170ae16f59bc901743",
+    "url": "/Todo/static/css/main.d875c6b0.chunk.css"
+  },
+  {
+    "revision": "a79a424f1016d6a656b8",
+    "url": "/Todo/static/js/2.5c0f57dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo/static/js/2.5c0f57dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb170ae16f59bc901743",
+    "url": "/Todo/static/js/main.6c563b61.chunk.js"
+  },
+  {
+    "revision": "162a65c0456d03b31aa2",
+    "url": "/Todo/static/js/runtime-main.e3579041.js"
+  }
+]);
